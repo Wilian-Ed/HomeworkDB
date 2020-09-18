36c68c5af5c92715061b0ff1a0c2aef7b2676adc
Wilian Ed <wilian122397@gmail.com>
2020-09-18 14:06:24 -0600

    tabla direcciones

--- PV_5pts/Scripts.sql
+++ PV_5pts/Scripts.sql
@@ -5,5 +5,19 @@ CREATE TABLE Departamento(
     Nombre VARCHAR(25) NOT NULL UNIQUE
     );
 --MUNICIPIO
-
+CREATE TABLE Municipio (
+    IdMunicipo INTEGER NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(25) NOT NULL,
+    IdDepartamento INTEGER NOT NULL,
+    CONSTRAINT FK_Departamento FOREIGN KEY (IdDepartamento) REFERENCES Departamento(IdDepartamento)
+    );
 --DIRECCION
+CREATE TABLE Direccion (
+    IdDireccion INTEGER NOT NULL PRIMARY KEY ,
+    IdMunicipio INTEGER NOT NULL,
+    Calle VARCHAR2(25),
+    Avenida VARCHAR2(25),
+    Casa VARCHAR2(10),
+    Zona INTEGER,
+    CONSTRAINT FK_Municipio FOREIGN KEY (IdMunicipio) REFERENCES Municipio(IdMunicipo)
+    );
