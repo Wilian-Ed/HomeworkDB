983f5b701c25c145c38d9e2d5c64ca565f725423
Wilian Ed <wilian122397@gmail.com>
2020-09-18 14:39:34 -0600

    Script todas las tablas agragadas

--- PV_5pts/Scripts.sql
+++ PV_5pts/Scripts.sql
@@ -21,3 +21,75 @@ CREATE TABLE Direccion (
     Zona INTEGER,
     CONSTRAINT FK_Municipio FOREIGN KEY (IdMunicipio) REFERENCES Municipio(IdMunicipo)
     );
+
+--CREACION DE PERSONAS PARA RELACION CON CLIENTES Y PROVEEDORES
+CREATE TABLE Personas (
+    IdPersona INTEGER NOT NULL PRIMARY KEY,
+    PrimerNombre VARCHAR2(15) NOT NULL,
+    SegundoNombre VARCHAR2(15) NOT NULL,
+    PrimerApellido VARCHAR2(10) NOT NULL,
+    SegundoApellido VARCHAR2(10) NOT NULL,
+    NumTelefono INTEGER,
+    IdDireccion INTEGER NOT NULL,
+    FOREIGN KEY (IdDireccion) REFERENCES Direccion(IdDireccion)
+    );
+
+CREATE TABLE Clientes (
+    IdCliente INTEGER NOT NULL PRIMARY KEY,
+    NIT VARCHAR2(12) NOT NULL,
+    IdPersona INTEGER NOT NULL,
+    FOREIGN KEY (IdPersona) REFERENCES Personas(IdPersona)
+    );
+
+CREATE TABLE Proveedores (
+    IdProveedor INTEGER NOT NULL PRIMARY KEY ,
+    Organizacion VARCHAR(50) NOT NULL,
+    Estado Varchar(1) DEFAULT 1,
+    IdPersona INTEGER NOT NULL,
+    FOREIGN KEY (IdPersona) REFERENCES Personas(IdPersona)
+    );
+
+CREATE TABLE FacturaVenta (
+    IdFacturaVenta INTEGER NOT NULL PRIMARY KEY ,
+    IdCliente INTEGER NOT NULL,
+    Fecha DATE NOT NULL,
+    FOREIGN KEY (IdCliente) REFERENCES Clientes(IdCliente)
+    );
+
+CREATE TABLE FacturaCompra (
+    IdFactura INTEGER NOT NULL PRIMARY KEY ,
+    IdProveedor INTEGER NOT NULL,
+    Fecha DATE NOT NULL,
+    FOREIGN KEY (IdProveedor) REFERENCES Proveedores(IdProveedor)
+    );
+
+CREATE TABLE DetalleCompras (
+    IdDetalleCompra INTEGER NOT NULL PRIMARY KEY,
+    IdFactura INTEGER NOT NULL,
+    IdProducto INTEGER NOT NULL,
+    Descripcion INTEGER NOT NULL,
+    Cantidad INTEGER NOT NULL,
+    Precio DECIMAL(9,2) NOT NULL,
+    FOREIGN KEY (IdFactura) REFERENCES FacturaCompra(IdFactura)
+    );
+
+CREATE TABLE DetalleVentas (
+    IdDetalleCompra INTEGER NOT NULL PRIMARY KEY ,
+    IdFacturaVenta INTEGER NOT NULL,
+    IdProducto INTEGER NOT NULL,
+    Cantidad INTEGER NOT NULL,
+    Precio DECIMAL NOT NULL,
+    FOREIGN KEY (IdFacturaVenta) references FacturaVenta(IdFacturaVenta),
+    FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
+    );
+
+CREATE TABLE Productos (
+    IdProducto INTEGER NOT NULL PRIMARY KEY ,
+    Nombre VARCHAR(25) NOT NULL,
+    Descripcion VARCHAR(255) NOT NULL,
+    CodigoProducto VARCHAR(8) NOT NULL UNIQUE ,
+    Canitdad INTEGER NOT NULL,
+    PrecioCompra DECIMAL(9,2) NOT NULL,
+    PrecioVenta DECIMAL(9,2) NOT NULL,
+    Estado VARCHAR(1) DEFAULT 1
+    );
